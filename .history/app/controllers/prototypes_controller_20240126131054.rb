7c0ba5bd8f5e9ffc2dc1e3cddaeb0ef117b5337c
Tanaka925 <tawashi259@gmail.com>
2024-01-26 15:46:45 +0900

    ユーザー管理機能の実装

---
+++ .history/app/controllers/prototypes_controller_20240126131054.rb
@@ -0,0 +1,8 @@
+class PrototypesController < ApplicationController
+  
+  def index
+  end
+
+  def configure_permitted_parameters
+  
+end
---
+++ .history/app/controllers/prototypes_controller_20240126131104.rb
@@ -0,0 +1,8 @@
+class PrototypesController < ApplicationController
+  
+  def index
+  end
+
+  def create
+ e
+end
